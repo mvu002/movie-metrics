0bb15b157103ef5f2b4d28baad2bfb8af4e8f3e1
Max Vu <mvu@berkeley.edu>
2023-01-17 21:05:41 -0800

    refined server.R code by using reactive variables rather than reusing same code chunk over and over. app currently works as a proof of concept -- shows top director, actor, actress, and their images.

--- code/app.R
+++ code/app.R
@@ -33,31 +33,64 @@ ui <- fluidPage(
       ), width = 12,
       
     # Output: Most watched movie genre ----
-    fluidRow(
-      textOutput("mostWatchedMovieGenre"),
-      tags$head(tags$style("#mostWatchedMovieGenre{color : red;
-                                                   font-size: 20px;
-                                                   font-style: italic;
-                           }")
-                )
-      ),
+    # fluidRow(
+    #   textOutput("mostWatchedMovieGenre"),
+    #   tags$head(tags$style("#mostWatchedMovieGenre{color : red;
+    #                                                font-size: 20px;
+    #                                                font-style: italic;
+    #                        }")
+    #             )
+    #   ),
+    
+    
 #    fluidRow(
 #      tableOutput("contents")
 #      )
+    # fluidRow(
+    #   textOutput("director_name"),
+    #   tags$head(tags$style("#director_name{color : black;
+    #                                   font-size: 20px;
+    #                                   font-style: italic;
+    #                        }")
+    #             )
+    # ),
     fluidRow(
-      textOutput("test_name"),
-      tags$head(tags$style("#test_name{color : black;
-                                      font-size: 20px;
-                                      font-style: italic;
-                           }")
-                )
+      align = "center",
+      textOutput("director_name"),
+         tags$head(tags$style("#director_name{color : black;
+                                         font-size: 20px;
+                                         font-style: bold;}")),
+      imageOutput("director_img")
+      ),
+
+headerPanel(""),
+headerPanel(""),
+headerPanel(""),
+headerPanel(""),
+
+    fluidRow(
+      align = "center", 
+      textOutput("actor_name"),
+      tags$head(tags$style("#actor_name{color : black;
+                                         font-size: 20px;
+                                         font-style: bold;}")),
+      imageOutput("actor_img")
     ),
+
+headerPanel(""),
+headerPanel(""),
+headerPanel(""),
+headerPanel(""),
+
     fluidRow(
-      imageOutput("director_img"),
-      #imageOutput("actor_img"),
-      #imageOutput("actress_img")
-    )
-    )
+      align = "center",
+      textOutput("actress_name"),
+      tags$head(tags$style("#actress_name{color : black;
+                                         font-size: 20px;
+                                         font-style: bold;}")),
+      imageOutput("actress_img")
+      )
+  )
 )
 
 
@@ -65,28 +98,98 @@ ui <- fluidPage(
 # Define server logic to read selected file ----
 server <- function(input, output) {
   
-  input_file <- reactive({input$file1$datapath})
+  ratings <- reactive({
+    fread(input$file1$datapath)
+    })
   
+  movie_ratings <- reactive({
+    filter(ratings(), `Title Type` == "movie")
+    })
   
+  ratings_and_data <- reactive({
+    inner_join(data, 
+               movie_ratings(),
+               by = c("tconst" = "Const")) %>%
+      select(-genres, -`Title Type`, -runtimeMinutes, -`Title`) %>%
+      clean_names()
+  })
   
+  # Identify top 5 movie directors
+  top_five_movie_directors <- reactive({
+    movie_directors <- filter(ratings_and_data(), category == "director")
+    movie_directors <- table(movie_directors$primary_name)
+    movie_directors <- data.frame(movie_directors)
+    colnames(movie_directors) <- c('Director', 'Count')
+    movie_directors <- as_tibble(movie_directors)
+    movie_directors <- arrange(movie_directors, desc(Count))
+    return(movie_directors[1:5, 1:2])
+  })
+  
+  # Select top movie director
+  top_movie_director <- reactive({
+    result <- top_five_movie_directors()
+    result[] <- lapply(result, as.character)
+    result <- toString(result[1, 1])
+    return(result)
+  })
+  
+  # Identify top 5 movie actors
+  top_five_movie_actors <- reactive({
+    movie_actors <- filter(ratings_and_data(), category == "actor")
+    movie_actors <- table(movie_actors$primary_name)
+    movie_actors <- data.frame(movie_actors)
+    colnames(movie_actors) <- c('Actor', 'Count')
+    movie_actors <- as_tibble(movie_actors)
+    movie_actors <- arrange(movie_actors, desc(Count))
+    return(movie_actors[1:5, 1:2])
+  })
+
+  # Select top movie actor
+  top_movie_actor <- reactive({
+    result <- top_five_movie_actors()
+    result[] <- lapply(result, as.character)
+    result <- toString(result[1, 1])
+    return(result)
+  })
+  
+  # Identify top 5 movie actresses
+  top_five_movie_actresses <- reactive({
+    movie_actresses <- filter(ratings_and_data(), category == "actress")
+    movie_actresses <- table(movie_actresses$primary_name)
+    movie_actresses <- data.frame(movie_actresses)
+    colnames(movie_actresses) <- c('Director', 'Count')
+    movie_actresses <- as_tibble(movie_actresses)
+    movie_actresses <- arrange(movie_actresses, desc(Count))
+    return(movie_actresses[1:5, 1:2])
+  })
+  
+  # Select top movie actress
+  top_movie_actress <- reactive({
+    result <- top_five_movie_actresses()
+    result[] <- lapply(result, as.character)
+    result <- toString(result[1, 1])
+    return(result)
+  })
+  
+  # Write top director/actor/actress to txt file
+  celebrity_list <- reactive({
+    names <- c(top_movie_director(), top_movie_actor(), top_movie_actress())
+    # fileConn<-file("../data/celebrity_names.txt")
+    # writeLines(names, fileConn)
+    # close(fileConn)
+    
+    return(names)
+  })
+
   output$mostWatchedMovieGenre <- renderText({
     
     req(input$file1)
-    ratings <- fread(input_file())
-
-    movie_ratings <- filter(ratings, `Title Type` == "movie")
-    ratings_and_data <- inner_join(data,
-                                   movie_ratings,
-                                   by = c("tconst" = "Const"))
-    ratings_and_data <- ratings_and_data %>%
-      select(-genres, -`Title Type`, -runtimeMinutes, -`Title`) %>%
-      clean_names()
 
     imdb_genres <- c('Action','Adult','Adventure','Animation','Biography','Comedy','Crime','Documentary','Drama','Family','Fantasy','Film Noir','Game Show','History','Horror','Musical','Music','Mystery','News','Reality-TV','Romance','Sci-Fi','Short','Sport','Talk-Show','Thriller','War','Western')
 
     genre_counts = c()
     for (genre in imdb_genres) {
-      genre_counts = c(genre_counts, sum(str_detect(movie_ratings$Genres, genre) == TRUE))
+      genre_counts = c(genre_counts, sum(str_detect(movie_ratings()$Genres, genre) == TRUE))
     }
 
     # Create df of movie genre counts
@@ -105,129 +208,74 @@ server <- function(input, output) {
     
   })
   
-  output$contents <- renderTable({
-    
+  output$director_name <- renderText({
     req(input$file1)
-    
-    # when reading semicolon separated files,
-    # having a comma separator causes `read.csv` to error
-    tryCatch(
-      {
-        df <- fread(input_file())
-      },
-      error = function(e) {
-        # return a safeError if a parsing error occurs
-        stop(safeError(e))
-      }
-    )
-    
-    return(head(df))
+    writeLines(celebrity_list(), "../data/celebrity_names.txt")
+    return(paste(top_movie_director(), "(most watched director)"))
   })
   
-  output$test_name <- renderText({
+  output$actor_name <- renderText({
     req(input$file1)
-    ratings <- fread(input_file())
-    
-    movie_ratings <- filter(ratings, `Title Type` == "movie")
-    ratings_and_data <- inner_join(data,
-                                   movie_ratings,
-                                   by = c("tconst" = "Const"))
-    ratings_and_data <- ratings_and_data %>%
-      select(-genres, -`Title Type`, -runtimeMinutes, -`Title`) %>%
-      clean_names()
-    
-    # Identify director whose movies I watched the most
-    movie_directors <- filter(ratings_and_data, category == "director")
-    
-    movie_directors <- table(movie_directors$primary_name)
-    movie_directors <- data.frame(movie_directors)
-    colnames(movie_directors) <- c('Director', 'Count')
-    movie_directors <- as_tibble(movie_directors)
-    
-    movie_directors <- arrange(movie_directors, desc(Count))
-    
-    # Select Top 5 directors
-    top_five_movie_directors <- movie_directors[1:5, 1:2]
-    top_five_movie_directors[] <- lapply(top_five_movie_directors, as.character)
-    top_movie_director <- toString(top_five_movie_directors[1, 1])
-    print(top_movie_director)
-    
-    
-    # Identify the actor whose movies I watched the most
-    movie_actors <- filter(ratings_and_data, category == "actor")
-    movie_actors 
-    
-    movie_actors <- table(movie_actors$primary_name)
-    movie_actors <- data.frame(movie_actors)
-    colnames(movie_actors) <- c('Actor', 'Count')
-    movie_actors <- as_tibble(movie_actors)
-    
-    movie_actors <- arrange(movie_actors, desc(Count))
-    
-    # Select Top 5 actors
-    top_five_movie_actors <- movie_actors[1:5, 1:2]
-    top_five_movie_actors[] <- lapply(top_five_movie_actors, as.character)
-    top_movie_actor <- toString(top_five_movie_actors[1, 1])
-    print(top_movie_actor)
-    
-    
-    # Identify actress whose movies I watched the most
-    movie_actresses <- filter(ratings_and_data, category == "actress")
-    
-    movie_actresses <- table(movie_actresses$primary_name)
-    movie_actresses <- data.frame(movie_actresses)
-    colnames(movie_actresses) <- c('Director', 'Count')
-    movie_actresses <- as_tibble(movie_actresses)
-    
-    movie_actresses <- arrange(movie_actresses, desc(Count))
-    
-    # Select Top 5 actresses
-    top_five_movie_actresses <- movie_actresses[1:5, 1:2]
-    top_five_movie_actresses[] <- lapply(top_five_movie_actresses, as.character)
-    top_movie_actress <- toString(top_five_movie_actresses[1, 1])
-    print(top_movie_actress)
-    
-    celebrity_list <- c(top_movie_director, top_movie_actor, top_movie_actress)
-    fileConn<-file("../data/celebrity_names.txt")
-    writeLines(celebrity_list, fileConn)
-    close(fileConn)
-    
-    py_run_file("tmdb_test.py")
-    
-    return(paste(top_movie_director, "(most watched director)"))
+    writeLines(celebrity_list(), "../data/celebrity_names.txt")
+    return(paste(top_movie_actor(), "(most watched actor)"))
   })
+  
+  output$actress_name <- renderText({
+    req(input$file1)
+    writeLines(celebrity_list(), "../data/celebrity_names.txt")
+    return(paste(top_movie_actress(), "(most watched actress)"))
+  })
+  
   
   output$director_img <- renderImage({
     
     req(input$file1)
-    ratings <- fread(input_file())
     
-    movie_ratings <- filter(ratings, `Title Type` == "movie")
-    ratings_and_data <- inner_join(data,
-                                   movie_ratings,
-                                   by = c("tconst" = "Const"))
-    ratings_and_data <- ratings_and_data %>%
-      select(-genres, -`Title Type`, -runtimeMinutes, -`Title`) %>%
-      clean_names()
+    # Use TMDb Python wrapper to get images
+    py_run_file("tmdb_test.py")
     
-    # Identify director whose movies I watched the most
-    movie_directors <- filter(ratings_and_data, category == "director")
+    print("posting director image")
     
-    movie_directors <- table(movie_directors$primary_name)
-    movie_directors <- data.frame(movie_directors)
-    colnames(movie_directors) <- c('Director', 'Count')
-    movie_directors <- as_tibble(movie_directors)
-    
-    movie_directors <- arrange(movie_directors, desc(Count))
-    
-    # Select Top 5 directors
-    top_five_movie_directors <- movie_directors[1:5, 1:2]
-    top_five_movie_directors[] <- lapply(top_five_movie_directors, as.character)
-    top_movie_director <- toString(top_five_movie_directors[1, 1])
-    print(top_movie_director)
-    
-    list(src = paste0("../data/", top_movie_director, ".jpg"))
+    list(src = paste0("../data/", top_movie_director(), ".jpg"))
   }, deleteFile = F)
+  
+  output$actor_img <- renderImage({
+    req(input$file1)
+    
+    print("posting actor image")
+    
+    list(src = paste0("../data/", top_movie_actor(), ".jpg"))
+  }, deleteFile = F)
+  
+  output$actress_img <- renderImage({
+    req(input$file1)
+    
+    # Use TMDb Python wrapper to get images
+    py_run_file("tmdb_test.py")
+    
+    print("posting actress image")
+    
+    list(src = paste0("../data/", top_movie_actress(), ".jpg"))
+  }, deleteFile = F)
+  
+  # output$contents <- renderTable({
+  #   
+  #   req(input$file1)
+  #   
+  #   # when reading semicolon separated files,
+  #   # having a comma separator causes `read.csv` to error
+  #   tryCatch(
+  #     {
+  #       df <- fread(input_file())
+  #     },
+  #     error = function(e) {
+  #       # return a safeError if a parsing error occurs
+  #       stop(safeError(e))
+  #     }
+  #   )
+  #   
+  #   return(head(df))
+  # })
   
 }
 
